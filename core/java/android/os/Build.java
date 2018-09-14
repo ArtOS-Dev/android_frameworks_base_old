b2cb80a5f5618b80b56ffc4f2baa044b08ac7dfa
maxwen <max.weninger@gmail.com>
2018-09-14 17:34:30 -0500

    Store artstar fingerprint for package data
    and use that to clear package cache instead of our
    recovery script solution
    
    Change-Id: I21f599a84f8ff28b53bc088f739374c037f81be0

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1002,6 +1002,19 @@ public class Build {
     public static final String FINGERPRINT = deriveFingerprint();
 
     /**
+     * ArtStarOS fingerprint
+     * @hide
+     */
+    public static final String PROP_ARTSTAR_FINGERPRINT = "ro.artstar.fingerprint";
+
+    /**
+     * ArtStarOS fingerprint
+     * @hide
+     */
+    public static final String ARTSTAR_FINGERPRINT = SystemProperties.get(PROP_ARTSTAR_FINGERPRINT,
+            deriveFingerprint());
+
+    /**
      * Some devices split the fingerprint components between multiple
      * partitions, so we might derive the fingerprint at runtime.
      */
